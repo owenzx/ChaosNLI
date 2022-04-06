6fc7264ee7ed4faea0b2a46c2c9d317cd9571b7a
owenzx <owen9608@outlook.com>
2022-04-06 16:25:01 -0400

    Revert "dist nli"
    This reverts commit 17a06b2cded84cf8b09046fe49b4a6729e604e25.

--- distnli/setup.sh
+++
@@ -1,7 +0,0 @@
-# Add current pwd to PYTHONPATH
-export DIR_TMP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-export PYTHONPATH=$PYTHONPATH:$DIR_TMP/src
-export PYTHONPATH=$PYTHONPATH:$DIR_TMP/utest
-
-echo PYTHONPATH=$PYTHONPATH
--- distnli/src/modeling/soft_crossentropy.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-def softCrossEntropy(input, target):
-    assert(len(input.shape)==2)
-    logprobs = torch.nn.functional.log_softmax(input, dim = -1)
-    return  -(target * logprobs).sum() / input.shape[0]
-
